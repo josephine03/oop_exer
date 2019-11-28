f7b8671c24234d87a412681cf32fcc5157a23a3a
josephine03 <57975730+josephine03@users.noreply.github.com>
2019-11-28 14:29:32 +0800

    Add files via upload

---
+++ vehicle.php
@@ -0,0 +1,85 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<center>
+<?php
+    class vehicle{
+
+    	//declaring member variable here
+    	var $vehicle;
+    	var $fuel;
+    	var $capacity;
+    	var $speed;
+
+
+    	//declaring member functions
+    	function setVehicle($vehicle){
+    		$this ->vehicle = $vehicle;
+    	}
+
+
+    	function getVehicle(){
+    		echo "vehicle type: $this->vehicle.<br/>";
+
+    	}
+    	  function setFuel($fuel){
+
+    	  	$this ->fuel = $fuel;
+			
+			}
+
+			function getFuel(){
+
+			echo "fuel type: $this->fuel.<br/>";
+    	}
+
+    		function setCapacity($cap){
+    			$this ->capacity =$cap;
+
+    	
+    		}
+
+    		function getCapacity(){
+    			
+    		echo"capacity type: $this->capacity.<br/>";
+    		}
+
+    		function setSpeed($speed){
+    			$this ->speed =$speed;
+    		}
+
+    		function getSpeed(){
+    			echo "speed type: $this->speed.<br/>";
+    		}
+
+
+    	}
+
+    	
+
+    	$type = new vehicle();
+    	$type->setVehicle(" grandia");
+    	$type->getVehicle();
+    	$type = new vehicle();
+    	$type->setFuel("teargaas");
+    	$type->getFuel();
+    	$type = new vehicle();
+    	$type->setCapacity(2);
+    	$type->getCapacity();
+		$type->setSpeed(100);
+    	$type->getSpeed();
+
+
+    	
+    	
+
+
+    	
+?>
+
+</center>
+</body>
+</html>
